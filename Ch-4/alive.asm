998801018031ada7b00a06bdb0367a737a66d35d
ItsThePlatypus <u1922081@live.warwick.ac.uk>
2021-12-24 15:59:20 +0000

    Added chapter 4

---
+++ Ch-4/alive.asm
@@ -0,0 +1,32 @@
+; alive.asm
+section .data
+    msg1    db  "Hello, world!",10
+    msg2    db  "Alive and kicking!",10
+    msg1Len equ $-msg1
+    msg2Len equ $-msg2
+    radius  dq 357
+    pi      dq 3.14
+section .bss
+section .text
+    global main
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg1
+    mov rdx, msg1Len
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg2
+    mov rdx, msg2Len
+    syscall
+
+    mov rsp, rbp
+    pop rbp
+    mov rax, 60
+    mov rdi, 0
+    syscall
