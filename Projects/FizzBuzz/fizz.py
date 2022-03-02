6ee9bb824c12b0e4b8bd0e27b5c852e00cceb00d
ItsThePlatypus <61837386+ItsThePlatypus@users.noreply.github.com>
2022-03-02 16:10:51 +0000

    Updated contents of file

--- Projects/FizzBuzz/fizz.py
+++ Projects/FizzBuzz/fizz.py
@@ -1,8 +1,4 @@
 n = 0
-<<<<<<< HEAD
-=======
-
->>>>>>> f378ccfdeda33cad756748179b17a760fc378cc0
 for n in range(1,101):
     if n % 15 == 0:
         print(f"{n} - FizzBuzz")
@@ -13,9 +9,4 @@ for n in range(1,101):
     else:
         print(f"{n} - Nothing")
     n += 1
-<<<<<<< HEAD
 print("Done")
-=======
-
-print("Done")
->>>>>>> f378ccfdeda33cad756748179b17a760fc378cc0
